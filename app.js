1437fa2fbaee767b0a3cfd16a1133365e429305f
Felix <felix.delrio.1@gmail.com>
2019-04-28 16:14:08 +0200

    .Aver

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require("express")
+var cors = require("cors")
 var bodyParser = require('body-parser')
 
 var app = express()
+
+var origen = {
+    origin : "https://mdcproyecto.herokuapp.com/",
+    op
+}
 
 // Cargar rutas
 var controls_router = require("./routes/routes")
@@ -11,6 +17,8 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 // configurar CORSE
+
+
 
 // rutas
 app.use("/", controls_router)
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose")
 var app = require("./app")
-var port = 3800; 
+var port = 3800;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc" ,  { useNewUrlParser: true }  )
-    .then( () => {
+mongoose.connect("mongodb+srv://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc", { useNewUrlParser: true })
+    .then(() => {
         console.log("Conexion establecida");
         app.listen(process.env.PORT || port, () => {
             console.log("El servidor esta corriendo en https://mdcproyecto.herokuapp.com/")
         })
-    }).catch( err => console.log(err) )
+    }).catch(err => console.log(err))
