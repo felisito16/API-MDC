a49c27833c8ed4cd3fec385c9c4e6001a22fef42
Felix <felix.delrio.1@gmail.com>
2019-04-28 17:32:23 +0200

    .Probando con mLab

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var app = require("./app")
 var port = 3800;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc?retryWrites=true", { useNewUrlParser: true })
+// mongodb://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc?retryWrites=true
+// mongodb://admin:MdC-7824@ds137687.mlab.com:37687/heroku_wqxg0rvb
+mongoose.connect("mongodb://admin:MdC-7824@ds137687.mlab.com:37687/heroku_wqxg0rvb", { useNewUrlParser: true })
     .then(() => {
         console.log("Conexion establecida");
-        app.listen(process.env.PORT || port, () => {
-            console.log("El servidor esta corriendo en https://mdcproyecto.herokuapp.com/")
+        app.listen(process.env.PORT || port, function() {
+            console.log("El servidor esta corriendo en https://mdcproyecto.herokuapp.com/");
         })
     }).catch(err => console.log(err))
