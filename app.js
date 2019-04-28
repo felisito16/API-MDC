4668c6c7ecadaa52ef76935133b87daa11ff8b4a
Felix <felix.delrio.1@gmail.com>
2019-04-28 13:15:14 +0200

    Ruta /getAllUsers

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var bodyParser = require('body-parser')
+
+var app = express()
+
+// Cargar rutas
+
+// body-parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// configurar CORSE
+
+// rutas
+app.get('/getAllUsers', (req, res) => {
+    res.status(200).send({
+        message: 'Ruta /getAllUsers'
+    })
+})
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var mongoose = require("mongoose")
+var app = require("./app")
+var port = 3800;
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb+srv://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/test" , { useNewUrlParser: true } )
     .then( () => {
         console.log("Conexion establecida");
+        app.listen(port, () => {
+            console.log("El servidor esta corriendo en https://mdcproyecto.herokuapp.com/")
+        })
 
-    }).catch(  err => console.log(err) )
+    }).catch( err => console.log(err) )
