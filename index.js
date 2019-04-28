8e91c558df007abf59f9d98ef084edfeaf9ec796
Felix <felix.delrio.1@gmail.com>
2019-04-28 16:34:44 +0200

    lo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = require("./app")
 var port = 3800;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc", { useNewUrlParser: true })
+mongoose.connect("mongodb://admin:MdC-7824@cluster0-98qru.gcp.mongodb.net/mdc?retryWrites=true", { useNewUrlParser: true })
     .then(() => {
         console.log("Conexion establecida");
         app.listen(process.env.PORT || port, () => {
