8c177b94038be74e6b17bd1a83ebd1265a99cec6
Felix <felix.delrio.1@gmail.com>
2019-04-28 16:16:19 +0200

    .csd

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var bodyParser = require('body-parser')
 
 var app = express()
 
-var origen = {
+/* var origen = {
     origin : "https://mdcproyecto.herokuapp.com/",
     op
-}
+} */
 
 // Cargar rutas
 var controls_router = require("./routes/routes")
