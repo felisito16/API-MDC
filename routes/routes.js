5d3c68d186ec49f51275cb0cf717d9d2ae3e508a
Luis <luisbarrigacarrasco@gmail.com>
2019-05-01 21:15:02 +0200

    Prueba POST 3

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ var api = express.Router()
 
 api.get("/prueba", UsersController.prueba)
 //api.get("/validarUsuario/:usuario/:pass", UsersController.validarUsuario)
-api.post("/validarUsuario", UsersController.validarUsuario)
+api.post("/validar", UsersController.validarUsuario)
 api.post("/save", UsersController.saveUsuario)
 
 module.exports = api;
