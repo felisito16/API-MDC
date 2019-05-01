9d29d6d05d00925028088ca63412228c084634a6
Luis <luisbarrigacarrasco@gmail.com>
2019-05-01 21:14:02 +0200

    Prueba POST 2

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,8 @@ var UsersController = require('../controls/controls')
 var api = express.Router()
 
 api.get("/prueba", UsersController.prueba)
-api.get("/validarUsuario/:usuario/:pass", UsersController.validarUsuario)
+//api.get("/validarUsuario/:usuario/:pass", UsersController.validarUsuario)
+api.post("/validarUsuario", UsersController.validarUsuario)
 api.post("/save", UsersController.saveUsuario)
 
 module.exports = api;
