7d4d47af4bb7fc884b0e32726a6a4b50f0da912a
Felix <felix.delrio.1@gmail.com>
2019-05-14 16:11:47 +0200

    ant v7 - Modelo matricula
    Añadido _id

--- models/matriculas.js
+++ models/matriculas.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 var matriculasSchema = Schema({
-
+    _id: {
+        $oid: String
+    },
     nombre_completo: {
         nombre: String,
         primer_apellido: String,
