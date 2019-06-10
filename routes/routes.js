45f21df981a8d7da5116b95fe949d6f3cafca08b
Felix <felix.delrio.1@gmail.com>
2019-06-10 14:39:28 +0200

    v1

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,6 @@ api.post("/save", UsersController.saveUsuario)
 api.post("/saveMatricula", UsersController.crearMatricula)
 api.post("/cargarMatriculas", UsersController.cargarMatriculas)
 api.delete("/deleteMatricula/:id", UsersController.deleteMatricula)
-api.put("/matricula/:id", UsersController.asignarMatricula)
+api.put("/asignarMatricula/:id", UsersController.asignarMatricula)
 
 module.exports = api;
