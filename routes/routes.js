7d223988e1af524176bacba38371c4ce19861265
Felix <felix.delrio.1@gmail.com>
2019-06-11 22:16:34 +0200

    v4

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,6 @@ var UsersController = require('../controllers/controls')
 
 var api = express.Router()
 
-api.get("/prueba", UsersController.prueba)
-api.get("/validarUsuario/:usuario/:pass", UsersController.validarUsuario)
 api.get("/matricula/:id", UsersController.consultarMatricula)
 // Inicio - 6 primeras pendientes y erroneas
 /* api.get("/6erroneas") */
